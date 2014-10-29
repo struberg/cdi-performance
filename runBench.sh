d684855a26d042f4f064e542079730e0ca9a0159
Mark Struberg <struberg@apache.org>
2014-10-29 10:23:34 +0100

    upgrade runBench.sh to include all tested
    run
    $> ./runBench.sh | tee mvn.log

--- runBench.sh
+++ runBench.sh
@@ -3,11 +3,23 @@
 echo "benching OWB-1.2.6"
 mvn clean install
 
-echo "benching OWB-1.5.0-SNAPSHOT"
-mvn test -POWB15 -Dowb.version=1.5.0-SNAPSHOT
+echo "benching OWB-1.2.0"
+mvn clean install -Dowb.version=1.2.0
+
+echo "benching OWB-1.1.6"
+mvn clean install -Dowb.version=1.1.6
+
+echo "benching OWB-1.1.8"
+mvn clean install -Dowb.version=1.1.8
+
+echo "benching OWB-1.5.0-SNAPSHOT (CDI-1.2)"
+mvn clean install -POWB15 -Dowb.version=1.5.0-SNAPSHOT
 
 echo "benching Weld-1.1.23.Final"
-mvn test -PWeld -Dweld.version=1.1.23.Final
+mvn clean install -PWeld -Dweld.version=1.1.23.Final
+
+echo "benching Weld-2.2.6.Final"
+mvn clean install -PWeld
 
 echo "benching Weld-2.2.5.Final"
-mvn test -PWeld -Dweld.version=2.2.5.Final
+mvn clean install -PWeld -Dweld.version=2.2.5.Final
